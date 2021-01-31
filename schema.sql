1666c11eb0d7b830c0e7c0afd341ade1cddbe4c9
Val Nimirenco <valnimirenco@gmail.com>
2021-01-30 19:33:36 -0800

    built schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_DB;
+CREATE database employees_DB;
+
+USE employees_DB;
+
+CREATE TABLE departament (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 4) NOT NULL,
+    departament_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (departament_id) REFERENCES departament(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
